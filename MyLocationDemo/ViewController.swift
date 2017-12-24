c3b58950f37d715416b7b0d1f00dd4a641cffade
William E. Seyler <wseyler@pentaho.com>
2017-12-23 20:22:46 -0500

    INITIAL CHECK-IN: Location is app is functional, logging location to the terminal and displaying it in the app.

--- MyLocationDemo/ViewController.swift
+++ MyLocationDemo/ViewController.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    struct ALocation {
+        var lat :Double
+        var long :Double
+        
+        func toDictionary() -> [String:Any] {
+            return ["lat":self.lat, "long":self.long]
+        }
+    }
+    let locationManager = CLLocationManager()
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeValue: UILabel!
+    @IBOutlet weak var addressValue: UILabel!
+    
+    @IBAction func getLocation(_ sender: Any) {
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +42,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // CLLocationManagerDelegate
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError: \(error)");
+        let errorAlert = UIAlertController(title:"Error", message:error.localizedDescription, preferredStyle:UIAlertControllerStyle.alert)
+        errorAlert.show(self, sender: nil)
+    }
 
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didUpdateLocations: \(locations)")
+        for location in locations {
+            longitudeValue.text = "\(location.coordinate.longitude)"
+            latitudeLabel.text = "\(location.coordinate.latitude)"
+            
+            let aLocation = ALocation(lat:location.coordinate.latitude, long:location.coordinate.longitude)
+            let jsonData  = try? JSONSerialization.data(withJSONObject: aLocation.toDictionary(), options:[])
+            if let JSONString = String(data: jsonData!, encoding: String.Encoding.utf8) {
+                print(JSONString)
+            }
+        }
+    }
+    
 }
 
